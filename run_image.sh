3804604fe20f8676e2a4723f6e1dba4dbb94a5cb
Chih Chiu <chih.chiu.19@gmail.com>
2017-12-07 19:01:40 -0800

    cleanup runner script

--- run_image.sh
+++ run_image.sh
@@ -5,9 +5,8 @@ function show_usage() {
     echo
     echo "------------------------------------------------------------"
     echo "Usage reference:"
-    echo "- Run without arugments to start Jupyter."
-    echo "- Run with any other arguments will execute them in the docker image environment."
-    echo "The external directory is mounted in the image under /workspace/external."
+    echo "*) Run without arugments to start Jupyter."
+    echo "*) Run with any other arguments will execute them in the docker image environment."
     echo "------------------------------------------------------------"
     echo
 }
@@ -26,6 +25,7 @@ if [ $# == 0 ]
 then
     echo
     echo "------------------------------------------------------------"
+    echo "The external directory is mounted under /workspace/external."
     echo "To quit Jupyter, press ctrl-c twice."
     echo "------------------------------------------------------------"
     echo
@@ -33,9 +33,8 @@ then
 else
     echo
     echo "------------------------------------------------------------"
-    echo "If you need to run a server, use port 8888 and it can be "
-    echo "accessed on host at http://${machine_ip}:8888"
+    echo "The external directory is mounted under /workspace/external."
     echo "------------------------------------------------------------"
     echo 
-    docker run -it -p 8888:8888 -v $(pwd)/external:/workspace/external "${image_name}" "$@"
+    docker run -it -v $(pwd)/external:/workspace/external "${image_name}" "$@"
 fi
