b340c437ebd1f856bc383f502b5b79bf5a9044b4
Chih Chiu <chih.chiu.19@gmail.com>
2017-12-11 08:41:13 -0800

    update run image

--- run_image.sh
+++ run_image.sh
@@ -5,8 +5,9 @@ function show_usage() {
     echo
     echo "------------------------------------------------------------"
     echo "Usage reference:"
-    echo "*) Run without arugments to start Jupyter."
-    echo "*) Run with any other arguments will execute them in the docker image environment."
+    echo "1) Run without arugments to start bash in docker."
+    echo "2) Run with subcommand \"notebook\" to start Jupyter."
+    echo "3) Run with other subcommands to run them in docker."
     echo "------------------------------------------------------------"
     echo
 }
@@ -22,6 +23,14 @@ else
 fi
 
 if [ $# == 0 ]
+then
+    echo
+    echo "------------------------------------------------------------"
+    echo "The external directory is mounted under /workspace/external."
+    echo "------------------------------------------------------------"
+    echo 
+    docker run -it -v $(pwd)/external:/workspace/external "${image_name}" bash
+elif [ $# == 1 ] && [ $1 == "notebook" ]
 then
     echo
     echo "------------------------------------------------------------"
