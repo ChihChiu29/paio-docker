894f40f63d7dfe8f926dd613cc2fdbf75e14b519
ChihChiu <chih.chiu.19@gmail.com>
2018-04-09 19:54:27 -0700

    shorten path in cloud9 ide

--- cloud9_ide_start.sh
+++ cloud9_ide_start.sh
@@ -8,4 +8,4 @@ echo "Access at port 8887."
 echo "To quit, use docker kill."
 echo "------------------------------------------------------------"
 echo
-docker run -it -d -p 8887:80 -v $(pwd)/external:/workspace/external "${image_name}"
+docker run -it -d -p 8887:80 -v $(pwd)/external/:/workspace/ "${image_name}"
