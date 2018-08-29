c850246675e59eada0e67a8c33b95a85e4766fca
chihchiu29 <chih.chiu.19@gmail.com>
2018-08-28 19:46:14 -0700

    switched to run_lab.sh

--- run_lab.sh
+++ run_lab.sh
@@ -7,8 +7,8 @@ echo "======================================================"
 # Start Cloud9 editor.
 ./run_cloud9_ide.sh
 
-# Start notebook image (blocking).
-./run_image.sh notebook
+# Start main image and run lab (blocking).
+./run_image.sh lab
 
 # Turn down all running images.
 ./utilities/stopall.sh
