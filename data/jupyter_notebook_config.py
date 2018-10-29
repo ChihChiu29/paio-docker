168a1d146562cf02eaaef8d072344d84d135c3ca
ChihChiu29 <chih.chiu.19@gmail.com>
2018-10-28 20:27:38 -0700

    update ip to fix new jupyter lab error

--- data/jupyter_notebook_config.py
+++ data/jupyter_notebook_config.py
@@ -1,6 +1,7 @@
 import os
 from IPython.lib import passwd
 
-c.NotebookApp.ip = '*'
+# See: https://github.com/jupyter/notebook/issues/3946
+c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = int(os.getenv('PORT', 8888))
 c.NotebookApp.open_browser = False
