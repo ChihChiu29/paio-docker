a3f1a18a76a3716548777864c4afb1e9809c68d8
chihchiu29 <chih.chiu.19@gmail.com>
2018-12-02 20:52:29 -0800

    update update_image script

--- update_image.sh
+++ update_image.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 docker pull "chihchiu29/paio-docker"
-docker pull "kdelfour/cloud9-docker"
+docker pull "chihchiu29/cloud9"
 
 # GC.
 for imageId in $(docker images | grep "<none>" | tr -s " " | cut -f 3-3 -d " ");
