b42979d5ef0525f85eccdd7cffc4e0dddc030b10
Chih Chiu <chih.chiu.19@gmail.com>
2019-07-04 18:43:29 -0700

    add instructions on how to create cronjobs to startme.sh

--- external/startme.sh
+++ external/startme.sh
@@ -1,7 +1,17 @@
 #! /usr/bin/env bash
 # This script is run automatically when the image is run (by run_image.sh).
 
-# Start cron jobs.
+# Start cron jobs from external/cronjobs.cron.
+#
+# 1. Create a single script to execute the command you want to execute.
+#   1a. Remember to use `date` and `>` to create logs for execution times and logs, example:
+#       #! /usr/bin/env bash
+#       cd /workspace/external/
+#       suffix=$(date +"%Y%m%d-%H%M%S")
+#       python3 -m stockfun.scripts.download_today_ratings > ./logs/download_today_ratings_logs_${suffix}.txt 2>&1
+# 2. Add the script to external/cronjobs. The following example executes a
+#    script every 6 hours and allows to open GUI:
+#    0 */6 * * * export DISPLAY=:0 && /bin/bash /workspace/external/stockfun/scripts/download_ratings_cron.sh
 service cron start
 cd /workspace/external
 if [ -e cronjobs.cron ]
@@ -9,7 +19,7 @@ then
   crontab cronjobs.cron
 fi
 
-# Process commands.
+# Process startup commands.
 if [ $# == 0 ]
 then
     bash
