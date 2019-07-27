0bad9f572fd4d3e59d5b05cc22c2aedeaf9c9afc
Chih Chiu <chih.chiu.19@gmail.com>
2019-07-26 21:37:17 -0700

    update remove old logs

--- external/logs/remove_old_logs.sh
+++ external/logs/remove_old_logs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Remove logs that are more than 7 days old:
-find /path/to/files/ -type f -name '*.txt' -mtime +7 -exec rm {} \;
+find . -type f -name '*.txt' -mtime +7 -exec rm {} \;
