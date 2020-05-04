ce60c89ba06af26108e4118884699cf0a844febd
Chih Chiu <chih.chiu.19@gmail.com>
2020-05-04 15:49:12 -0700

    add new command

---
+++ run_image_with_cloud9.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+echo "======================================================"
+echo "  WARNING: WILL STOP ALL RUNNING IMAGES AFTERWARDS    "
+echo "======================================================"
+
+# Start Cloud9 editor.
+./run_cloud9_ide.sh
+
+# Start main image and run lab (blocking).
+./run_image.sh $@
+
+# Turn down all running images.
+./utilities/stopall.sh
